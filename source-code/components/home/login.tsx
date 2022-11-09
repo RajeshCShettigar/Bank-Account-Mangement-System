410189c4aaa0f75f268011e8c8c3b556742cf803
Rajesh <Rajeshcshettigar@gmail.com>
2022-11-09 21:51:50 +0530

    Update

---
+++ source-code/components/home/login.tsx
@@ -0,0 +1,22 @@
+const Login = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>Welcome to Bank of PRK</h1>
+        <div className="service-box">
+          <div className="services" id="admin">
+            <a href="google.com">Admin Login</a>
+          </div>
+          <div className="services" id="register">
+            <a href="google.com">New Customer</a>
+          </div>
+          <div className="services" id="netbanking">
+            <a href="google.com">Net Banking</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
